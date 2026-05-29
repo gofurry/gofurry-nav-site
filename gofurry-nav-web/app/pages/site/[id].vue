cf3b1487763386180c79fa3d0470e86575f87319
福狼 <faolanthewooolf@gmail.com>
2026-05-29 23:57:19 +0800

    feat(web): add nav v2 site detail dev page

--- gofurry-nav-web/app/pages/site/[id].vue
+++ gofurry-nav-web/app/pages/site/[id].vue
@@ -1,7 +1,12 @@
 <template>
-  <SiteDetailRoutePage />
+  <SiteV2DetailDevPage v-if="isDevRoute" />
+  <SiteDetailRoutePage v-else />
 </template>
 
 <script setup lang="ts">
 import SiteDetailRoutePage from '@/components/site/SiteDetailRoutePage.vue'
+import SiteV2DetailDevPage from '@/components/site/SiteV2DetailDevPage.vue'
+
+const route = useRoute()
+const isDevRoute = computed(() => route.path.replace(/\/$/, '').endsWith('/dev'))
 </script>
