5a28c930be054c3ec26bba2eb578b10968476456
福狼 <faolanthewooolf@gmail.com>
2026-05-30 01:57:37 +0800

    fix(web): restore dev site detail route

--- gofurry-nav-web/app/pages/site/[id]/[domain].vue
+++ gofurry-nav-web/app/pages/site/[id]/[domain].vue
@@ -1,7 +1,11 @@
 <template>
-  <SiteDetailRoutePage />
+  <NuxtPage v-if="isDevRoute" />
+  <SiteDetailRoutePage v-else />
 </template>
 
 <script setup lang="ts">
 import SiteDetailRoutePage from '@/components/site/SiteDetailRoutePage.vue'
+
+const route = useRoute()
+const isDevRoute = computed(() => route.path.replace(/\/$/, '').endsWith('/dev'))
 </script>
